c7c423a48d02c7efe1b621fa6e9e0f0b9212fb35
Cristiano <cristianosampaio16@gmail.com>
2025-05-11 15:55:38 -0300

    chore: typo

--- src/test/java/com/techelevator/VendingMachineVertexTests.java
+++ src/test/java/com/techelevator/VendingMachineVertexTests.java
@@ -11,7 +11,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class VendingMachineVertexTest implements VendingMachineGeneral, VendingMachineBuyingMenu {
+public class VendingMachineVertexTests implements VendingMachineGeneral, VendingMachineBuyingMenu {
     private VendingMachine testVM;
     private int salesReportCount = 0;
     private int currentItemStock = 0;
